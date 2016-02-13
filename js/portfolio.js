9f38cfef2c71abd03c85786955ed72dab6db50c1
Colin Harkness <Colin@Colins-MacBook-Air.local>
2016-02-13 21:43:11 +1100

    basic layout

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,3 @@
 $(document).ready(function () {
-	var  bubble = function(event) {
-		// console.log(event.clientX, event.clientY)
-		var $b = $('<div/>').addClass('bubble');
 
-		var size = Math.random() * 4;
-
-		$b.css({
-			width: size + 'em',
-			length: size + 'em',
-			left: event.clientX,
-			top: event.clientY
-		});
-		$('body').append($b)
-
-	;
-		setTimeout(function(){
-			$b.animate({
-				top: -1000
-			}, 2000, function(){
-			$b.remove();
-		});	
-		}, 1400);
-	};
-
-	$(window).on("mousemove", bubble);
 });
